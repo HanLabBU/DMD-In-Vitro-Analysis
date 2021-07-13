b04bc2cd253382111172806bf9b6abd9d10707be
Pierre Fabris <fabrispierre@gmail.com>
2021-07-12 22:02:58 -0400

    Plotting with modular exclusion of trials in both conditions

--- debug_bleaching.m
+++ debug_bleaching.m
@@ -17,9 +17,12 @@ cd('~/handata_server/Pierre Fabris/DMD Project/All In Vitro Analysis/');
 save_fig_path = '~/handata_server/Pierre Fabris/DMD Project/Data Figures/';
 
 % Ignore the first trial across each FOV for photobleaching estimation
-ignore_first = 1;
+ignore_first = 0;
 % Ignore the 2nd trial of 24
-ignore_24 = 1;
+ignore_24 = 0;
+
+% Ignore only first trials of the DMD condition
+ignore_first_dmd = 1;
 
 % Original Sampling Frequency
 sample_frequency = 500; % Hz
@@ -252,22 +255,22 @@ for id=1:length(wideloc)
         end
     end
     
-  else
-    
-      if ignore_24 == 1 & contains(indifile.allresults.fov_name, '24') == 1
-        % Ignore the second trial of the bleaching values
-        indi_temp = indifile.allresults.bleach([1, 3:end], mROI(:, 1));
-        wide_temp = widefile.allresults.bleach([1, 3:end], mROI(:, 1));
-      else
+    % Ignoring only the DMD trial
+  elseif ignore_first_dmd == 1
+               
         % Store all of the bleaching values
-        indi_temp = indifile.allresults.bleach(:, mROI(:, 1));
-        wide_temp = widefile.allresults.bleach(:, mROI(:, 1));
-      end
+        indi_temp = indifile.allresults.bleach(2:end, mROI(:, 1));
+        wide_temp = widefile.allresults.bleach(1:end, mROI(:, 1));
+
   end
-  
+
     %DEBUG figure out what is going on with the Fluorescence decay plot
-    if size(indi_temp < 0.90) > 1
+    if nansum(indi_temp < 0.90) > 0
         disp(['Large bleach trial ' ses(indiloc(id)).name]);
+    end
+    
+    if nansum(wide_temp < 0.90) > 0
+        disp(['Large bleach trial ' ses(wideloc(id)).name]);
     end
 
   indi_temp = (1 - indi_temp).*-100;   %./indi_tr_lengths;
@@ -285,10 +288,18 @@ end
 % title('Photodecay line plot');
 
 %M=[ ((1-(indiB)).*-1)'  ,((1-(wideB)).*-1)'].*100;
+%figure();
+%M=[ indi_Bdiff', wide_Bdiff'];
+%boxplot(M, {'Individual DMD', 'Wide Field'},  'notch','on',   'colors',[ 0.4 0.4 0.4], 'symbol','.k');
+%title('Raw difference between initial and end part of trace');
+
+disp('Photobleaching Statistics');
+[h,p,ci,stats] = ttest(indiAllB, wideAllB)
+
 figure();
-M=[ indi_Bdiff', wide_Bdiff'];
+M=[ indiAllB', wideAllB'];
 boxplot(M, {'Individual DMD', 'Wide Field'},  'notch','on',   'colors',[ 0.4 0.4 0.4], 'symbol','.k');
-title('Raw difference between initial and end part of trace');
+title('Photobleaching');
 
 %figure();
 %M=[ horzcat_pad(indi_Bdiff', indi_Bdiff_norm')];
